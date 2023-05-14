a21db7a4dcb0e61ebf03b393d2b101d31a18daec
FLX777 <subtirica.florin29@gmail.com>
2023-05-14 16:20:13 +0300

    b

--- cycleDetection.h
+++ cycleDetection.h
@@ -3,15 +3,17 @@
 #include <string.h>
 #include <stdbool.h>
 
+#include "stack.h"
+
 #define MAX_N 100 // dimensiunea maxima a grafului
 
 typedef struct node {
-    int val;
-    struct node* next;
+    size_t val;
+    struct node *next;
 } node;
 
 void add_edge(size_t u, size_t v);
 
-size_t is_cyclic_util(size_t u);
+bool is_cyclic_util(size_t u);
 
-size_t is_cyclic(size_t n);
+bool is_cyclic(size_t n);
